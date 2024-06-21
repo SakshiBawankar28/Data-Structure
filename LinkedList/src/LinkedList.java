af9f0cd4d85b24cc775671273234e37ffb89da2b
SakshiBawankar28 <sakshibawabkar@gmail.com>
2024-06-21 23:23:03 +0530

    Linked List

---
+++ LinkedList/src/LinkedList.java
@@ -0,0 +1,165 @@
+public class LinkedList {
+	private Node head;
+
+	public LinkedList() {
+		head = null;
+	}
+
+	// insert method
+	public boolean insert(int data) 
+	{
+		// Creates a new node with the given data.s
+		Node newNode = new Node(data);
+
+		// Checks if the node creation failed and returns false
+		if (newNode == null) {
+			return false;
+		}
+
+		// If the list is empty (head is null), sets the new node as the head and
+		// returns true.
+		if (head == null) {
+			head = newNode;
+			return true;
+		}
+
+		// Initializes a temporary node temp to traverse the list.
+		Node temp = head;
+		// if list is not empty then traverse the list to find the last node
+		while (temp.getNext() != null) {
+			temp = temp.getNext();
+			// Checks if the head node contains the data to be inserted. If it does, returns
+			// false (to prevent duplicate values).
+			if (temp.getData() == data) {
+				return false;
+			}
+		}
+		// set new node as the next node of last node
+		temp.setNext(newNode);
+		return true;
+	}
+
+	
+	// insert method at specific position
+	public boolean insertAtPosition(int data, int position) 
+	{
+		//If the position is invalid (less than or equal to 0) or 
+		//if the position is beyond the first position in an empty list, returns false.
+		if(position <= 0  || head == null  && position > 1)
+		{
+			return false;
+		}
+		
+		// Creates a new node with the given data.s
+		Node newNode = new Node(data);
+
+		// Checks if the node creation failed and returns false
+		if (newNode == null) {
+			return false;
+		}
+		
+		//insert newNode at position one
+		if(position == 1)
+		{
+			newNode.setNext(head);
+			head = newNode;
+			return true;
+		}
+		
+		//Initializes a prev node to find the node after which the new node is to be inserted.
+		Node prev = head;
+		for(int i=1; i<position-1; i++)
+		{
+			prev = prev.getNext();
+			if(prev == null)
+			{
+				return false;
+			}
+		}
+		//Sets the next of the new node to the node currently at the target position.
+		newNode.setNext(prev.getNext());
+		//Sets the next of the prev node to the new node, effectively inserting it into the list.
+		prev.setNext(newNode);
+		return true;
+
+	}
+
+	// display method
+	/*
+	 * The display method:
+		Starts from the head of the linked list.
+		Iterates through each node until it reaches the end (null).
+		Prints the data of each node followed by a space.
+		Moves to the next line after printing all the node data.
+	 */
+	public void display() {
+		Node temp = head;
+		while (temp != null) {
+			System.out.print(temp.getData() + " ");
+			temp = temp.getNext();
+		}
+		System.out.println();
+	}
+	
+	
+	//delete method by value
+	public boolean deleteByVal(int data) 
+	{
+		//Checks if the list is empty. If so, it returns false.
+        if(head == null) {
+            return false;
+        }
+
+        //Checks if the head node contains the value to be deleted.
+        //If so, it updates the head and returns true.
+        if(head.getData() == data) {
+            head = head.getNext();
+            return true;
+        }
+
+        Node prev = head, del = head;
+        /*
+         * Otherwise, it traverses the list to find the node with the specified value.
+			If it finds the node, it removes it by updating the previous node's next pointer to skip the node to be deleted and returns true.
+			If it doesn't find the node, it returns false
+         */
+        while(del.getData() != data) {
+            prev = del;
+            del = del.getNext();
+            if(del == null) {
+                return false;
+            }
+        }
+
+        prev.setNext(del.getNext());
+        return true;
+    }
+	
+	//delete method by position
+	public boolean deleteByPosition(int position)
+	{
+		if(head == null  ||  position < 0)
+		{
+			return false;
+		}
+		if(position == 1)
+		{
+			head = head.getNext();
+			return true;
+		}
+		Node prev = head;
+		for(int i=1; i<position-1; i++)
+		{
+			prev = prev.getNext();
+			if(prev.getNext() == null)
+			{
+				return false;
+			}
+		}
+		Node del = prev.getNext();
+		prev.setNext(del.getNext());
+		return false;
+		
+	}
+}
+
---
+++ LinkedList/src/Main.java
@@ -0,0 +1,46 @@
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		LinkedList li = new LinkedList();
+		
+		li.insert(10);
+		li.insert(20);
+		li.insert(30);
+		li.insert(40);
+		li.insert(50);
+		
+		li.display();
+		
+		System.out.println("\nInsert data by position");
+		li.insertAtPosition(8000, 1);
+		li.insertAtPosition(4000, 5);
+		li.insertAtPosition(1000, 8);
+		li.insertAtPosition(12000,12);
+		li.display();
+		
+		System.out.println("\nDelete by value");
+		li.deleteByVal(8000);
+		li.display();
+		li.deleteByVal(4000);
+		li.display();
+		li.deleteByVal(1000);
+		li.display();
+		
+		System.out.println();
+		li.insertAtPosition(8000, 1);
+		li.insertAtPosition(4000, 5);
+		li.insertAtPosition(1000, 8);
+		li.insertAtPosition(12000,12);
+		li.display();
+		System.out.println("Delete by position");
+		li.deleteByPosition(1);
+		li.display();
+		li.deleteByPosition(4);
+		li.display();
+		li.deleteByPosition(6);
+		li.display();
+	}
+
+}
